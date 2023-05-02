b0986d597d4cba01da5e4277e42df34284a04373
kafka-0817 <kaede19090@gmail.com>
2023-05-02 23:34:11 +0800

    创建项目

---
+++ src/main/java/com/kafka/carbongraphvisual/CarbonGraphVisualApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.carbongraphvisual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbonGraphVisualApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarbonGraphVisualApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kafka/carbongraphvisual/CarbonGraphVisualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.carbongraphvisual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbonGraphVisualApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
